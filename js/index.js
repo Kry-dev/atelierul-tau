723468fe234523ad91a98a8f6d26c9bea699de2a
kry-dev <yaroslavkryvda@gmail.com>
2018-06-20 20:16:23 +0300

    fix scroll to top

--- js/index.js
+++ js/index.js
@@ -144,6 +144,7 @@ $(document).ready(function () {
         e.preventDefault();
         $('.filter-box').slideUp( "300");
     });
+    $('.scrollTop-container').hide();
     $(window).scroll(function(){
         if ($(this).scrollTop() > $(".main-products").height() - 100) {
             $('.scrollTop-container').show();
